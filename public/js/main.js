7aaf0f7b6512d76546a93e8fd4e98d9cad2ad4f3
andersonmcook <andersonmcook@gmail.com>
2016-03-04 11:02:24 -0600

    ux improvements, cannot play without starting game, input ends at game end

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,15 @@
 // ;(function () {
 //   console.log('hey')
 
-let array
+let array = []
 let counter = 0
 let mark = 'x'
 
-// sets up game
+// sets up game and add event listener
 function startGame () {
-  array = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+  array = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  board.addEventListener('click', makeMove)
+  message.innerHTML = "x starts"
 }
 
 // check to see if a winning move has been made
@@ -20,27 +22,42 @@ function winCheck (array) {
           (array[3] === array[4] && array[4] === array[5]) ||
           (array[6] === array[7] && array[7] === array[8]) ||
           (array[2] === array[4] && array[4] === array[6])
-          ? "somebody won" : "keep going"
+          ? true : false
 }
 
 // make a move
 function makeMove () {
-  // console.log('click')
-  let mark
+  let turn
+// check whose move it is based on the counter and tell them
+  counter % 2 === 0 ? (mark = 'x', turn = 'o') : (mark = 'o', turn ='x')
+  message.innerHTML = `${turn}'s turn`
+// if the space is already taken, alert user
+  if (array[event.target.id] === 'x' || array[event.target.id] === 'o') {
+    message.innerHTML = 'Pick a different space'
+  } else {
+// get the space array index from DOM
+    const space = document.getElementById(event.target.id)
+// visually mark it
+    space.innerHTML = mark
+// add it to array
+    array[event.target.id] = mark
+// check win conditions and end or increment counter
+    winCheck(array) === true ? endGame() : counter++
+  }
+}
+
+// end game and remove event listener
+function endGame() {
   counter % 2 === 0 ? mark = 'x' : mark = 'o'
-  console.log(event.target.id)
-  const space = document.getElementById(event.target.id)
-  // console.log(space)
-  space.innerHTML = mark
-  array[event.target.id] = mark
-  winCheck(array)
-  counter += 1
+  message.innerHTML = `${mark} won!`
+  board.removeEventListener('click', makeMove)
 }
 
 // click
 const form = document.querySelector('form')
 const board = document.querySelector('table')
-board.addEventListener('click', makeMove)
+const message = document.querySelector('#message')
+// board.addEventListener('click', makeMove)
 
 // start new game
 form.addEventListener('submit', () => {
